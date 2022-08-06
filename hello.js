588ef4abf5c4911dc1692952c5414f4632fbaa51
Jerrylgy <2983545564@qq.com>
2022-08-06 15:20:21 +0800

    third commit

--- hello.js
+++ hello.js
@@ -5,11 +5,12 @@ option = null;
 option = {
     title: {
         text: '小刘的世界',
-       subtext:'今天天气正好',
+       subtext:'13 to go',
         left: 'center',
     },
     tooltip: {
-        trigger: 'item'
+        trigger: 'item',
+        formatter: '地区：{b}<br/>次数：{c}'
     },
     legend: {
         orient: 'vertical',
@@ -19,12 +20,13 @@ option = {
     visualMap: {
         type: 'piecewise',
         pieces: [
-            
-            {value:1,label: '×'},
- 
-            {value:0,label: '√'}
+            {value:0,label: '还没去过哦'},            
+            {value:1,label: '一次'},
+            {value:2,label: '两次'},
+            {value:999,label: '好多次'},
+
         ],
-        color: ['#d74f3d','#f3ca7e']
+        color: ['#d74f3d','#e0620d','#ea9518','#f3ca7e']
     },
     toolbox: {
         show: true,
@@ -47,7 +49,7 @@ option = {
     },
     series: [
         {
-            name: '冬至吃什么',
+            name: '记录',
             type: 'map',
             mapType: 'china',
             roam: false,
@@ -63,41 +65,41 @@ option = {
                 }
               },
             data: [
-                {name: '湖北',value: 0},
-                {name: '湖南',value: 0},
-                {name: '江西',value: 0},
-                {name: '安徽',value: 0},
+                {name: '湖北',value: 1},
+                {name: '湖南',value: 999},
+                {name: '江西',value: 1},
+                {name: '安徽',value: 1},
                 {name: '云南',value: 1},
-                {name: '广东',value: 0},
-                {name: '福建',value: 1},
-                {name: '广西',value: 0},
-                {name: '浙江',value: 0},
-                {name: '江苏',value: 0},
-                {name: '上海',value: 0},
-                {name: '海南',value: 1},
-                {name: '香港',value: 0},
-                {name: '澳门',value: 1},
-                {name: '台湾',value: 1},
-                {name: '贵州',value: 0},
-                {name: '重庆',value: 0},
-                {name: '宁夏',value: 1},
-                {name: '四川',value: 0},
-                {name: '新疆',value: 1},
-                {name: '西藏',value: 1},
-                {name: '青海',value: 1},
-                {name: '甘肃',value: 0},
-                {name: '内蒙古',value: 1},
-                {name: '陕西',value: 0},
-                {name: '北京',value: 0},
-                {name: '辽宁',value: 1},
-                {name: '黑龙江',value: 1},
-                {name: '哈尔滨',value: 1},
-                {name: '山西',value: 0},
-                {name: '山东',value: 0},
-                {name: '河北',value: 0},
-                {name: '河南',value: 0},
-                {name: '吉林',value: 1},
-                {name: '天津',value: 0},
+                {name: '广东',value: 999},
+                {name: '福建',value: 0},
+                {name: '广西',value: 1},
+                {name: '浙江',value: 1},
+                {name: '江苏',value: 1},
+                {name: '上海',value: 2},
+                {name: '海南',value: 0},
+                {name: '香港',value: 2},
+                {name: '澳门',value: 0},
+                {name: '台湾',value: 0},
+                {name: '贵州',value: 1},
+                {name: '重庆',value: 1},
+                {name: '宁夏',value: 0},
+                {name: '四川',value: 1},
+                {name: '新疆',value: 0},
+                {name: '西藏',value: 0},
+                {name: '青海',value: 0},
+                {name: '甘肃',value: 999},
+                {name: '内蒙古',value: 0},
+                {name: '陕西',value: 1},
+                {name: '北京',value: 2},
+                {name: '辽宁',value: 0},
+                {name: '黑龙江',value: 0},
+                {name: '哈尔滨',value: 0},
+                {name: '山西',value: 1},
+                {name: '山东',value: 1},
+                {name: '河北',value: 1},
+                {name: '河南',value: 1},
+                {name: '吉林',value: 0},
+                {name: '天津',value: 1},
                 
             ]
         }
